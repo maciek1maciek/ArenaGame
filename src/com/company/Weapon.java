8f033f47492e7863a7dae2097d92f14eb4f31bbb
Maciek Koczyk <maciejkoczyk@gmail.com>
2021-02-08 12:05:00 +0100

    creating class weapon-done

---
+++ src/com/company/Weapon.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Weapon extends Stuff {
+
+    private int baseAttack;
+
+    public Weapon(String name, String weight) {
+        super(name, weight);
+    }
+
+
+}
